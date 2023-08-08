bfd4f775d8a385d99c2d59f18381952f7f71ed4c
unknown <sujivraj1204@gmail.com>
2023-08-08 12:28:20 +0530

    Change config file 4

--- train.sh
+++ train.sh
@@ -2,4 +2,4 @@
 
 CONFIG=$1
 
-torchrun --nproc_per_node=1 --master_port=4321 basicsr/train.py -opt $CONFIG --launcher pytorch
+torchrun --nproc_per_node=2 --master_port=4321 basicsr/train.py -opt $CONFIG --launcher pytorch
